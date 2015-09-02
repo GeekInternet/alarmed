842f23b680f4d20fbed9ed9f550fa81f5bad179e
Keith Wade <keawade@gmail.com>
2015-09-02 00:50:14 -0500

    Electron implemented
    Currently running a simple hello world.

---
+++ app.js
@@ -0,0 +1,25 @@
+var app = require('app')
+var BrowserWindow = require('browser-window');
+
+// Report crashes to the server
+require('crash-reporter').start();
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.openDevTools();
+  mainWindow.on('closed', function(){
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
